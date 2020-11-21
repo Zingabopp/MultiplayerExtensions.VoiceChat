fc2b2edc483972384b4467dd292d668817bc69b6
Zingabopp <Zingabopp@gmail.com>
2020-11-21 13:05:15 -0600

    Added IVoiceChatActivity interface

---
+++ MultiplayerExtensions.VoiceChat/IVoiceChatActivity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MultiplayerExtensions.VoiceChat
+{
+    public interface IVoiceChatActivity
+    {
+        IConnectedPlayer Player { get; }
+        event EventHandler<bool>? TalkingStateChanged;
+        event EventHandler<bool>? MutedStateChanged;
+    }
+}

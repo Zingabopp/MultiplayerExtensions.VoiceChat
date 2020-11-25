a12d5e19c6cb693fcbd9824be13b30aaa29aff72
Zingabopp <Zingabopp@gmail.com>
2020-11-25 03:30:20 -0600

    Removed Checksum, error handling on deserialize

--- MultiplayerExtensions.VoiceChat/Networking/VoipDataPacket.cs
+++ MultiplayerExtensions.VoiceChat/Networking/VoipDataPacket.cs
@@ -11,7 +11,6 @@ namespace MultiplayerExtensions.VoiceChat.Networking
         public int DataLength;
         public byte[]? Data;
         public int Index;
-        public int Checksum;
         private bool ArrayRented;
 
         protected static PacketPool<VoipDataPacket> Pool
@@ -30,7 +29,6 @@ namespace MultiplayerExtensions.VoiceChat.Networking
             //packet.PlayerId = playerId;
             packet._packetVersion = 1;
             packet.Index = index;
-            packet.Checksum = 0;
             packet.Data = data;
             packet.DataLength = dataLength;
             return packet;
@@ -40,25 +38,34 @@ namespace MultiplayerExtensions.VoiceChat.Networking
 
         public void Deserialize(NetDataReader reader)
         {
-            ArrayRented = true;
-            //PlayerId = reader.GetString();
-            _packetVersion = reader.GetByte();
-            Index = reader.GetInt();
-            Checksum = reader.GetInt();
-            DataLength = reader.GetInt();
-            if (DataLength > 0)
+            try
             {
-                Data = ByteAryPool.Rent(DataLength);
-                reader.GetBytes(Data, 0, DataLength);
-                int checksum = GetChecksum(Data, DataLength);
-                if (checksum != Checksum)
-                    Plugin.Log?.Warn($"Expected checksum didn't match.");
-#if DEBUG
-                Plugin.Log?.Debug($"Receiving: {Index}|{DataLength}|{Checksum}:{checksum}|{Data[0]}:{Data[DataLength - 1]}");
-#endif
+                _packetVersion = reader.GetByte();
+                Index = reader.GetInt();
+                DataLength = reader.GetInt();
+                if (DataLength > 1024)
+                {
+                    Plugin.Log?.Warn($"DataLength of '{DataLength}' is higher than expected, dropping packet.");
+                    DataLength = 0;
+                    Data = Array.Empty<byte>();
+                }
+                else if (DataLength > 0 && DataLength <= reader.AvailableBytes)
+                {
+                    ArrayRented = true;
+                    Data = ByteAryPool.Rent(DataLength);
+                    reader.GetBytes(Data, 0, DataLength);
+                }
+                else
+                {
+                    Plugin.Log?.Warn($"Unable to parse packet. DataLength: {DataLength} | Available bytes: {reader.AvailableBytes}");
+                    DataLength = 0;
+                    Data = Array.Empty<byte>();
+                }
             }
-            else
-                Data = Array.Empty<byte>();
+            catch (Exception ex)
+            {
+                Plugin.Log?.Debug(ex);
+            }
         }
 
         public void Serialize(NetDataWriter writer)
@@ -66,13 +73,11 @@ namespace MultiplayerExtensions.VoiceChat.Networking
             //writer.Put(PlayerId);
             writer.Put(_packetVersion);
             writer.Put(Index);
-            int checksum = GetChecksum(Data, DataLength);
-            writer.Put(checksum);
             if (Data != null)
             {
                 writer.PutBytesWithLength(Data, 0, DataLength);
 #if DEBUG
-                Plugin.Log?.Debug($"Sending: {Index}|{DataLength}|{checksum}|{Data[0]}:{Data[DataLength - 1]}");
+                Plugin.Log?.Debug($"Sending: {Index}|{DataLength}|{Data[0]}:{Data[DataLength - 1]}");
 #endif
             }
             else
@@ -82,25 +87,13 @@ namespace MultiplayerExtensions.VoiceChat.Networking
             }
         }
 
-        private static int GetChecksum(byte[]? data, int dataLength)
-        {
-            if (data == null)
-                return 0;
-            int checksum = 0;
-            for (int i = 0; i < dataLength; i++)
-            {
-                unchecked
-                {
-                    checksum += data[i];
-                }
-            }
-            return checksum;
-        }
-
         public void Release()
         {
             if (ArrayRented)
+            {
                 ByteAryPool.Return(Data);
+                Data = null;
+            }
             Pool.Release(this);
         }
     }
